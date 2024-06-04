cb316c98f05ef9d5f8a361b5efd3d7967dcecf00
dreadpiraterobertsss <bniftalizade@gmail.com>
2024-06-04 17:47:43 +0400

    Add files via upload
    cses grid path dp

---
+++ main (79).cpp
@@ -0,0 +1,44 @@
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+
+int main(){
+    const int mod =  1e9+7;
+    int n;
+    cin >> n;
+    vector<string> arr(n);
+    for(int i = 0 ; i < n; i++){
+        cin >> arr[i];
+        
+    }
+    
+    vector<vector<int>> dp(n, vector<int> (n,0));
+    
+    if (arr[0][0] == '.'){
+        dp[0][0] = 1;
+    }
+    
+    for(int i = 0; i < n ; i++){
+        for(int j = 0; j < n; j++){
+            if (arr[i][j] == '*'){
+                dp[i][j] = 0;
+            }
+            else{
+                if (j > 0){
+                    dp[i][j] = (dp[i][j] + dp[i][j-1]) % mod;
+                }
+                if (i > 0){
+                    dp[i][j] = (dp[i][j] + dp[i-1][j]) % mod;
+                }
+            }
+        } 
+    }
+    
+    cout << dp[n-1][n-1];
+    return 0;
+}
+
+
